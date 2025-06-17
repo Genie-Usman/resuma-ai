78bd0fa55b80768d688d36c5f63f35da01b79b36
Muhammad Usman <155563664+Genie-Usman@users.noreply.github.com>
2025-06-17 22:31:02 +0500

    Implement routing and layout structure for main application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+// Components
+import EditResume from './pages/ResumeUpdate/EditResume';
+import Dashboard from './pages/Home/Dashboard';
+import LandingPage from "./pages/LandingPage"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/resume/:resumeId' element={<EditResume />} />
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          styles: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </>
   )
 }
 
